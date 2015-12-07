e855f9ed7604acfdb78dd388944d8051f9580f1e
June Shaw <june.shaw@me.com>
2015-12-07 11:50:20 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 
 $("button").click(function(){
 
+  // object of css values, time, function to call when done.
+  // $(".dim").animate({"color": "#229911", "opacity": 1.0}, 1000, function() {console.log("done");});
+  //
+  genTrack();
+
 });
+
+function genTrack() {
+  var timer = 0;
+  playlist["results"].forEach(function (trackElement, indexElement) {
+    var myId="song" + indexElement;
+    $('#container').append('<div id='+myId + ' class = "dim"> '+  trackElement["trackName"] + '</div>');
+    var myDiv = $("#"+myId);
+    myDiv.delay(timer).animate({"opacity": 1.0}, 1000);
+    timer += 50;
+
+  })};
