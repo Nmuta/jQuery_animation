fb1a8d6e7e34dee011572054f9c2185974f1c778
Ben Ebel <bge1234@gmail.com>
2015-12-07 12:03:07 -0700

    added Google Maps API AJAX

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 $("button").click(function(){
+  //User input for API search
+  var getter = $.ajax({
+    url: "https://maps.googleapis.com/maps/api/geocode/json?address=Oxford%20University,%20uk",
+    method: "GET",
+    dataType: "json"
+  });
+
   var timer = 0;
-  for(var i = 0 ; i < playlist["results"].length; i++) {
-    $("#tracknames").append("<li class='dim'>" + playlist["results"][i]["trackName"] + "</li>")
-    $("li").delay(timer).animate({"opacity": 1.0}, 1000);
-    timer += 25;
-  }
+
+  getter.done(function(data) {
+    // console.log(data["results"][0]["address_components"][i]["long_name"]);
+    for(var i = 0 ; i < data["results"][0]["address_components"].length; i++) {
+      console.log(data["results"][0]["address_components"][i]["long_name"]);
+      $("ul").append("<li class='dim'>" + data["results"][0]["address_components"][i]["long_name"] + "</li>")
+      $("li").delay(timer).animate({"opacity": 1.0}, 1000);
+      timer += 25;
+    }
+  });
 });
