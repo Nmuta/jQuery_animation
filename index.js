fe5fa281ea867cd2cd41298b667edac4adb787cd
Josh Silverstein <silverjosh90@gmail.com>
2015-12-07 13:42:02 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-
-$("button").click(function(){
-
+$("button").click(function() {
+  // Object of css values, time, function
+  $(".dim").animate({
+    "opacity": 1.0
+  }, 1000)
+  var derp = playlist["results"]
+  var timer = 0
+  derp.forEach(function(e, i) {
+    $('#container').append("<div class='dim' id='" + i + "'><p>" + e["trackName"] + "</p></div>")
+    $("#" + i).delay(timer).animate({
+      "opacity": 1.0
+    }, 1000)
+    timer+= 50
+  })
 });
