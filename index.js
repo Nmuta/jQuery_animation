e55b98e8350f054a283c3a9f436494d452e663db
Ben Ebel <bge1234@gmail.com>
2015-12-07 11:45:33 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 $("button").click(function(){
+  var timer = 0;
   for(var i = 0 ; i < playlist["results"].length; i++) {
-    $("#tracknames").append("<li>" + playlist["results"][i]["trackName"] + "</li>");
+    $("#tracknames").append("<li class='dim'>" + playlist["results"][i]["trackName"] + "</li>")
+    $("li").delay(timer).animate({"opacity": 1.0}, 1000);
+    timer += 25;
   }
 });
