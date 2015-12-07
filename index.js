882eef8994b9f106cddd14f873dbf98f51c89cc4
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-07 11:51:10 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
 $("button").click(function(){
+  // object of css values, time, function
+  var timer = 0
 
+  for (r of playlist.results) {
+
+    $('#container').append('<div id="' + r['trackId'] +'">' + r['trackName'].toUpperCase() + '</div>');
+
+    $('#'+r['trackId']).delay(timer+=50).animate({'opacity': 1}, 500)
+  }
 });
