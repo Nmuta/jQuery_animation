fba3ee9f136fd980ea3b9f419ee06c14f9700ba5
Scott Christensen <scottmc526@gmail.com>
2015-12-07 13:32:52 -0700

    google api animation

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
+$(document).ready(function(){
 
-$("button").click(function(){
+  $("#add").click(function(){
+    var getter = $.ajax ({
+      url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Oxford%20University,%20uk&sensor=false',
+      method: 'GET',
+      dataType: 'json'
+    })
+  getter.done(function(returned){
+  $('.dim').animate({'opacity':1.0,'font-family':'impact', 'font-size':'30px'}, 1000, function(){})
+  var results = returned['results'][0]['address_components']
+  var timer = 0;
+  results.forEach(function (obj, i) {
+
+    var my_id = "long_name"+ i;
+    $('#container').append('<div id='+my_id+' class="dim" >'+results[i]['long_name']+'</div>')
+
+    var my_div = $('#'+my_id);
+    my_div.delay(timer).animate({'opacity': 1.0}, 1000, function(){});
+    timer += 100;
+    })
+
+  })
+  $('#remove').click(function (){
+    $('#container').find('div').remove()
+  })
 
 });
+})
