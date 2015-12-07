13529e63effef40ead365d83286b9132bbbdce32
Mikel <mikelbrierly@gmail.com>
2015-12-07 16:05:57 -0700

    completed jQuery animations lesson

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-
+$(document).ready(function(){
 $("button").click(function(){
-  //object of CSS values, time, function to call when done.
-  $(".dim").animate({"opacity": 1.0, "font-size": "30px"}, 1000, function(){
 
-    results = playlist["results"];
-      var timer = 0;
-    playlist["results"].forEach(function(e,i){
-      var my_id = "song"+i;
-      $("#container").append('<div id='+my_id+' class="dim">"'+ results[i]["trackName"] + '"</div>');
+    var ajax_call = $.ajax({
+      url: "http://swapi.co/api/starships/?format=json",
+      method: "GET",
+      dataType: "json"
+  });
+
+  var timer = 0;
+  ajax_call.done(function(response){
+    for (var i = 0; i < response["results"].length; i++){
+      var my_id = "ship"+i;
+      $("#container").append('<div id='+my_id+' class="dim">"' + response["results"][i]["name"] + '"</div>');
       var my_div = $("#"+my_id);
-      my_div.delay(timer).animate({"opacity": 1.0}, 1000)
-      timer += 10;
+        my_div.delay(timer).animate({"opacity": 1.0}, 1000)
+        timer += 15;
+      }
     })
   })
 });
