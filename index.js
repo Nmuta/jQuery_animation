9fb4137dd7414b8007c8155610844a5f780fc54e
Mikel <mikelbrierly@gmail.com>
2015-12-07 11:49:04 -0700

    jquery lesson we completed in class together

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
 $("button").click(function(){
+  //object of CSS values, time, function to call when done.
+  $(".dim").animate({"opacity": 1.0, "font-size": "30px"}, 1000, function(){
 
+    results = playlist["results"];
+      var timer = 0;
+    playlist["results"].forEach(function(e,i){
+      var my_id = "song"+i;
+      $("#container").append('<div id='+my_id+' class="dim">"'+ results[i]["trackName"] + '"</div>');
+      var my_div = $("#"+my_id);
+      my_div.delay(timer).animate({"opacity": 1.0}, 1000)
+      timer += 10;
+    })
+  })
 });
