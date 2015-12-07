894bf7798ba9adfa409effae58fa78ca40d13e94
Brandon Jewell <brandon.royce.jewell@gmail.com>
2015-12-07 13:42:00 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
 $("button").click(function(){
+  var hey = playlist["results"]
+
+  var timer = 0
+
+  for (var i = 0; i < hey.length; i++) {
+    $("#container").append("<div  class='dim'id='beyonce"+i+"'><img src='"+hey[i]["artworkUrl30"]+"'/>"+"<br>"+hey[i]["trackName"]+"</div>")
+    var mydiv= $("#beyonce"+i)
+    mydiv.delay(timer).animate({"opacity":1.0}, 1000)
+    timer +=100
+  }
 
 });
